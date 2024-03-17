6cc00ddd297d262a6d43a373d5f97beecc68c23d
David-Elkabas <david.elkabass@gmail.com>
2024-03-17 11:38:30 +0200

    add the condition of cpu<80 before the program itself

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import subprocess
 from pypsexec.client import Client
 import csv
 from datetime import datetime
+import re
+
 
 executable = "whoami.exe"  # where the exe that need to be run on the client is locate
 csv_file = "ip_addresses.csv"
@@ -16,26 +18,46 @@ class IpAddress:
         self.working_dir = working_dir
         self.status = ""
         self.time_of_check = ""
+        self.cpu = ""
+        self.storage_by_percent = ""
+        self.storage_by_value = ""
+
 
     def __str__(self):
-        return f"MyClass: ip={self.ip}, username={self.username}, password={self.password}, status={self.status}, time_of_check={self.time_of_check} "
+        return f"MyClass: arguments={self.arguments}, ip={self.ip}, username={self.username}, password={self.password}, status={self.status}, time_of_check={self.time_of_check} "
 
 def read_from_csv(csv_file):
     data = []
     with open(csv_file, 'r', newline='') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
             ip = row['ip']
             username = row['username']
             password = row['password']
-            machine = IpAddress(ip, username, password, executable)
-            result2 = ps_info_script(machine.ip)
-            if result2:
-                result = ps_exec_script(machine)
+            executable = "wmic"  # where the exe that need to be run on the client is locate
+            arguments = "cpu get loadpercentage"
+            machine = IpAddress(ip, username, password, executable, arguments)
 
+            # send the request for the cpu usage and calculate to average of it
+            result = ps_exec_script(machine)
+            if result:
+                array_of_cpu_percentage = get_numbers_from_string(result[0])
+                cpu_percentage = given_array_calc_average(array_of_cpu_percentage)
+                machine.cpu = cpu_percentage
 
-            print(result)
-            machine.status = 'good' if result else 'bad'
+                #     machine.storage_by_percent = machine_info.storage_by_percent
+                #     machine.storage_by_value = machine_info.storage_by_value
+
+                if cpu_percentage < 80:  # need to add also the storage data for this section!
+                    machine.executable = "whoami.exe"
+                    machine.arguments =""
+                    result = ps_exec_script(machine)
+                    print(result)
+                    machine.status = 'good' if result else 'bad'
+                else:  # the cpu is too high - > dont execute the program
+                    machine.status = 'cpu issue'
+            else:
+                machine.status = 'not found'
             machine.time_of_check = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             data.append(machine)
     return data
@@ -43,7 +65,7 @@ def read_from_csv(csv_file):
 
 def write_to_csv(csv_file, array_of_updated_machines_to_save):
     fieldnames = ['ip', 'username', 'password', 'status', 'time_of_check']
-    with open(csv_file, 'w', newline='') as file:
+    with open("google.csv", 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         for machine in array_of_updated_machines_to_save:
@@ -57,9 +79,27 @@ def write_to_csv(csv_file, array_of_updated_machines_to_save):
         print("finish saving all the DATA")
 
 
-def ps_info_script(ip)
-    print("help")
-    #get the info about the storage and cpu from the machine
+def get_numbers_from_string(input_string):
+
+    # Regular expression pattern to match numbers
+    pattern = r'\d+'
+    # Find all numbers in the string using regex
+    numbers = re.findall(pattern, input_string)
+    # Convert the numbers from string to integers
+    numbers = [int(num) for num in numbers]
+    return numbers
+
+
+def given_array_calc_average(array_of_numbers):
+
+    counter = 0
+    sum = 0
+    for number in array_of_numbers:
+        counter = counter + 1
+        sum = sum + number
+
+    return sum/counter
+
 
 def ps_exec_script(machine):
     c = Client(machine.ip, username=machine.username, password=machine.password,
@@ -82,14 +122,22 @@ def ps_exec_script(machine):
     # print("STDERR:\n%s" % result[1].decode('utf-8') if result[1] else "")
     # print("RC: %d" % result[2])
     # return result
-    return  result[0].decode('utf-8')
+    # print(result)
+    return [result[0].decode('utf-8'), result[1].decode('utf-8'), result[2]]
 
 
 if __name__ == '__main__':
 
-    all_data = read_from_csv(csv_file)
-    for data in all_data:
-        print(data)
+    machine = IpAddress("172.20.10.9", "david", "Aa123456", "wmic", "cpu get loadpercentage")
+    result = ps_exec_script(machine)
 
-    write_to_csv(csv_file, all_data)
+    array_of_cpu_percentage = get_numbers_from_string(result[0])
+    cpu_percentage = given_array_calc_average(array_of_cpu_percentage)
+
+
+    all_machines_new_data = read_from_csv(csv_file)
+    for data in all_machines_new_data:
+        print(data)
+    #
+    # write_to_csv(csv_file, all_machines_new_data)
 
