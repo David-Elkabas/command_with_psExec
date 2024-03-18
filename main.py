910c65a3421ff2f6e83ed88fdd0a0af6ca7c4fb4
David-Elkabas <david.elkabass@gmail.com>
2024-03-18 13:49:14 +0200

    checking the target machine cpu percentage before running the main function

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import re
 
 executable = "whoami.exe"  # where the exe that need to be run on the client is locate
 csv_file = "ip_addresses.csv"
+csv_output = "output.csv"
 
 class IpAddress:
     def __init__(self, ip, username, password, executable, arguments="", working_dir="C:\Windows\System32"):
@@ -42,30 +43,31 @@ def read_from_csv(csv_file):
             result = ps_exec_script(machine)
             if result:
                 array_of_cpu_percentage = get_numbers_from_string(result[0])
-                cpu_percentage = given_array_calc_average(array_of_cpu_percentage)
-                machine.cpu = cpu_percentage
+                # there may be more than one core and therefore we need to calculate the average of them.
+                machine.cpu = given_array_calc_average(array_of_cpu_percentage)
 
                 #     machine.storage_by_percent = machine_info.storage_by_percent
                 #     machine.storage_by_value = machine_info.storage_by_value
-
-                if cpu_percentage < 80:  # need to add also the storage data for this section!
+                print(f"the cpu of {machine.ip} is {machine.cpu}%")
+                if machine.cpu < 80:  # need to add also the storage data for this section!
                     machine.executable = "whoami.exe"
                     machine.arguments =""
                     result = ps_exec_script(machine)
-                    print(result)
-                    machine.status = 'good' if result else 'bad'
+                    print(f"psExec succeed at {machine.ip} and the results are {result} \n")
+
+                    machine.status = 'all good' if result else 'psExec problem'
                 else:  # the cpu is too high - > dont execute the program
-                    machine.status = 'cpu issue'
+                    machine.status = f"cpu too full at {machine.cpu}% \n"
             else:
-                machine.status = 'not found'
+                machine.status = 'machine not found'
             machine.time_of_check = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             data.append(machine)
     return data
 
 
 def write_to_csv(csv_file, array_of_updated_machines_to_save):
-    fieldnames = ['ip', 'username', 'password', 'status', 'time_of_check']
-    with open("google.csv", 'w', newline='') as file:
+    fieldnames = ['ip', 'username', 'password', 'status', 'time_of_check', 'cpu']
+    with open(csv_file, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         for machine in array_of_updated_machines_to_save:
@@ -74,7 +76,9 @@ def write_to_csv(csv_file, array_of_updated_machines_to_save):
                 'username': machine.username,
                 'password': machine.password,
                 'status': machine.status,
-                'time_of_check': machine.time_of_check
+                'time_of_check': machine.time_of_check,
+                'cpu': machine.cpu
+
             })
         print("finish saving all the DATA")
 
@@ -102,6 +106,7 @@ def given_array_calc_average(array_of_numbers):
 
 
 def ps_exec_script(machine):
+    print(f"starting to connect to {machine.ip} with the command {machine.executable}")
     c = Client(machine.ip, username=machine.username, password=machine.password,
                encrypt=True)
     try:  # try to connect to the ip
@@ -111,11 +116,12 @@ def ps_exec_script(machine):
             result = c.run_executable(machine.executable, arguments=machine.arguments)
         finally:
             c.remove_service()
-            # c.cleanup() #In the case of a fatal failure, this project may leave behind some the PAExec payload in C:\Windows or the service still installed. so this line clean it
+            # c.cleanup()  #In the case of a fatal failure, this project may leave behind some the PAExec payload in
+            # C:\Windows or the service still installed. so this line clean it
             c.disconnect()
     except:  # didnt succeed to connect to the ip
         result = ""
-        print("Failed to connect to:" + machine.ip)
+        print("Failed to connect to:" + machine.ip + "\n")
         return result
 
     # print("STDOUT:\n%s" % result[0].decode('utf-8') if result[0] else "")
@@ -128,16 +134,18 @@ def ps_exec_script(machine):
 
 if __name__ == '__main__':
 
-    machine = IpAddress("172.20.10.9", "david", "Aa123456", "wmic", "cpu get loadpercentage")
-    result = ps_exec_script(machine)
-
-    array_of_cpu_percentage = get_numbers_from_string(result[0])
-    cpu_percentage = given_array_calc_average(array_of_cpu_percentage)
-
+    # - - - - - - - for debugging the cpu only - - - - - - - - - - - - - - -
+    # machine = IpAddress("172.20.10.9", "david", "", "wmic", "cpu get loadpercentage")
+    # result = ps_exec_script(machine)
+    #
+    # array_of_cpu_percentage = get_numbers_from_string(result[0])
+    # # there may be more than one core and therefore we need to calculate the average of them.
+    # cpu_percentage = given_array_calc_average(array_of_cpu_percentage)
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     all_machines_new_data = read_from_csv(csv_file)
     for data in all_machines_new_data:
         print(data)
-    #
-    # write_to_csv(csv_file, all_machines_new_data)
+
+    write_to_csv(csv_output, all_machines_new_data)
 
